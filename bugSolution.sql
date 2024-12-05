fb5ae3e35251b34715920fcab65ebc221933bd72
BugHunter-X <askzenai@gmail.com>
2024-12-06 00:38:04 +0100

    Add bugSolution.sql file

---
+++ bugSolution.sql
@@ -0,0 +1,7 @@
+To avoid the syntax error, choose a non-reserved keyword for your column alias.
+
+```sql
+SELECT column1 AS column1_alias FROM my_table;
+```
+
+Here, `column1_alias` is a more appropriate alias that won't clash with SQL's reserved keywords.
